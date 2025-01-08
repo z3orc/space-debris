fe628d54e825343df0c4184df8fbf658c353df55
z3orc <z3orc@users.noreply.github.com>
2025-01-08 23:06:53 +0100

    feat: include more values in global game state, add deinit method to state

--- src/state.zig
+++ src/state.zig
@@ -1,16 +1,21 @@
 const std = @import("std");
+const rl = @import("raylib");
+
 const entity = @import("entity/entity.zig");
 
 pub const State = struct {
     player: entity.Player,
-    asteroids: []entity.Asteroid,
-    asteroidQueue: std.DoublyLinkedList(entity.Asteroid),
+    asteroids: std.ArrayList(entity.Asteroid),
+    asteroidQueue: std.ArrayList(entity.Asteroid),
+    activeAsteroids: usize,
+    maxActiveAsteroids: usize,
+    deathSound: rl.Sound,
+    debug: bool = false,
+    gameActive: bool,
+    gameEndTime: f64,
 
-    pub fn init() State {
-        return .{
-            .player = undefined,
-            .asteroids = undefined,
-            .asteroidQueue = undefined,
-        };
+    pub fn deinit(self: *State) void {
+        self.asteroids.deinit();
+        self.asteroidQueue.deinit();
     }
 };
